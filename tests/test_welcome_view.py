c4b8488e85445759ce9e33563d7b06c6c47b3dc6
Saitroy <gleb.herz@yandex.ru>
2026-06-04 16:40:35 +0500

    feat: simplify the Home start screen

--- tests/test_welcome_view.py
+++ tests/test_welcome_view.py
@@ -20,24 +20,53 @@ class WelcomeViewTests(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls._app = QApplication.instance() or QApplication([])
 
-    def test_dashboard_reflows_to_one_column_at_compact_width(self) -> None:
+    def test_home_prioritizes_actions_recent_projects_runtime_and_examples(self) -> None:
+        view = WelcomeView(LocalizationService("en"))
+
+        self.assertFalse(view._hero_card.isHidden())  # noqa: SLF001
+        self.assertFalse(view._recent_card.isHidden())  # noqa: SLF001
+        self.assertFalse(view._runtime_card.isHidden())  # noqa: SLF001
+        self.assertFalse(view._examples_card.isHidden())  # noqa: SLF001
+        self.assertTrue(view._status_card.isHidden())  # noqa: SLF001
+        self.assertTrue(view._quick_start_card.isHidden())  # noqa: SLF001
+
+    def test_home_wide_layout_places_recent_projects_beside_runtime_and_examples(self) -> None:
         view = WelcomeView(LocalizationService("en"))
         view.resize(1100, 760)
         view.show()
         self._app.processEvents()
 
-        wide_status_position = view._dashboard_grid.getItemPosition(  # noqa: SLF001
-            view._dashboard_grid.indexOf(view._status_card)  # noqa: SLF001
+        recent_position = view._dashboard_grid.getItemPosition(  # noqa: SLF001
+            view._dashboard_grid.indexOf(view._recent_card)  # noqa: SLF001
         )
-        self.assertEqual(wide_status_position[:2], (0, 1))
+        runtime_position = view._dashboard_grid.getItemPosition(  # noqa: SLF001
+            view._dashboard_grid.indexOf(view._runtime_card)  # noqa: SLF001
+        )
+        examples_position = view._dashboard_grid.getItemPosition(  # noqa: SLF001
+            view._dashboard_grid.indexOf(view._examples_card)  # noqa: SLF001
+        )
+        self.assertEqual(recent_position[:4], (0, 0, 2, 1))
+        self.assertEqual(runtime_position[:2], (0, 1))
+        self.assertEqual(examples_position[:2], (1, 1))
 
+    def test_home_compact_layout_stacks_recent_runtime_and_examples(self) -> None:
+        view = WelcomeView(LocalizationService("en"))
         view.resize(900, 700)
+        view.show()
         self._app.processEvents()
 
-        compact_status_position = view._dashboard_grid.getItemPosition(  # noqa: SLF001
-            view._dashboard_grid.indexOf(view._status_card)  # noqa: SLF001
+        recent_position = view._dashboard_grid.getItemPosition(  # noqa: SLF001
+            view._dashboard_grid.indexOf(view._recent_card)  # noqa: SLF001
         )
-        self.assertEqual(compact_status_position[:2], (1, 0))
+        runtime_position = view._dashboard_grid.getItemPosition(  # noqa: SLF001
+            view._dashboard_grid.indexOf(view._runtime_card)  # noqa: SLF001
+        )
+        examples_position = view._dashboard_grid.getItemPosition(  # noqa: SLF001
+            view._dashboard_grid.indexOf(view._examples_card)  # noqa: SLF001
+        )
+        self.assertEqual(recent_position[:2], (0, 0))
+        self.assertEqual(runtime_position[:2], (1, 0))
+        self.assertEqual(examples_position[:2], (2, 0))
 
 
 if __name__ == "__main__":
